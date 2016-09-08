0ebb355fd0196be2b1f94137f85ea1edb82035e6
Neha Tandon <nehatandon1@gmail.com>
2016-09-08 12:43:30 +0530

    [WIP]

---
+++ strings_problems.rb
@@ -0,0 +1,16 @@
+# 01: Create an empty string using the String class
+
+String.new('')
+# Solution : empty_string = ''
+# or String.new
+
+# 02: Modify the following code so that double-quotes are used instead of single-quotes.
+puts 'It\'s now 12 o\'clock.'
+# Solution: 
+puts "It's now 12 o'clock." 
+# %Q(it's 12'o clock)
+# %q(its's 12'o clock)
+
+# 03: Using the following code, compare the value of name with the string 'RoGeR' while ignoring the case of both strings.
+# Print true if the values are the same; print false if they aren't.
+# Then, perform the same case-insensitive comparison, except compare the value of name with the string 'DAVE' instead of 'RoGeR'
