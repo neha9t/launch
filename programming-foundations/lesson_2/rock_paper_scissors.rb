3b2464a5d70d934de97af6862c30b956176387d4
Neha Tandon <nehatandon1@gmail.com>
2016-09-28 22:49:31 +0530

    Change variable Scope

--- programming-foundations/lesson_2/rock_paper_scissors.rb
+++ programming-foundations/lesson_2/rock_paper_scissors.rb
@@ -24,22 +24,14 @@ def counter(player_count, computer_count, player, computer)
   if win?(player, computer)
     player_count = player_count + 1
     puts "player : "+ player_count.to_s
+    player_count
+    binding.pry
   elsif win?(computer, player)
     computer_count = computer_count + 1
     puts "computer : " + computer_count.to_s
+    computer_count
   else
     prompt("It's a tie!")
-  end
-  total_count(player_count,computer_count)
-end
-
-def total_count(player_count, computer_count)
-  if player_count == 5
-    puts "player : "+ player_count.to_s
-    prompt_message("You Won")
-  elsif computer_count == 5
-    puts "computer : "+ computer_count.to_s
-    prompt_message("Computer Won")
   end
 end
 
@@ -56,7 +48,8 @@ end
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
-
+player_count = 0
+computer_count = 0
 loop do
   player_choice = ''
   loop do
@@ -84,11 +77,18 @@ loop do
 
   computer_choice = VALID_CHOICES.sample
   prompt("You chose: #{player_choice} ; Computer chose: #{computer_choice}")
-  player_count = 0
-  computer_count = 0
-  counter(player_count, computer_count, player_choice, computer_choice)
 
+
+  counter(player_count, computer_count, player_choice, computer_choice)
+   if player_count == 5
+    prompt("You Won!")
+  elsif computer_count == 5
+    prompt("Computer Won!")
+  end
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
 end
+
+
+
