9d1945c8d8a817a6f93c70d8ff512e47776e0565
Neha Tandon <nehatandon1@gmail.com>
2016-10-04 10:08:11 +0530

    Refactor #win? method logic

--- programming-foundations/lesson_2/rock_paper_scissors.rb
+++ programming-foundations/lesson_2/rock_paper_scissors.rb
@@ -2,59 +2,55 @@
 require 'pry'
 
 VALID_CHOICES = %w(rock paper scissors lizard spock)
+WINNING_CHOICES = {rock: ['scissors', 'lizard'], paper: %w(rock spock),
+ scissors: %w(paper lizard),
+  lizard: %w(spock paper),
+   spock: %w(scissors rock)}
 
-def test_method
-  prompt('test message')
+@player_count = 0
+@computer_count = 0
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper') ||
-    (first == 'rock' && second == 'lizard') ||
-    (first == 'lizard' && second == 'spock') ||
-    (first == 'spock' && second == 'scissors') ||
-    (first == 'scissors' && second == 'lizard') ||
-    (first == 'lizard' && second == 'paper') ||
-    (first == 'paper' && second == 'spock') ||
-    (first == 'spock' && second == 'rock')
+  WINNING_CHOICES[first].include?(second)
 end
 
-def counter(player_count, computer_count, player, computer)
+def shortcut_check(choice)
+  case choice.downcase
+  when 'r', 'rock' then 'rock'
+  when 's', 'scissors' then 'scissors'
+  when 'p', 'paper' then 'paper'
+  when 'l', 'lizard' then 'lizard'
+  when 'sp', 'spock' then 'spock'
+  end
+end
+
+def win_counter(player, computer)
   if win?(player, computer)
-    player_count = player_count + 1
-    puts "player : "+ player_count.to_s
-    player_count
-    binding.pry
+    @player_count += 1
+    puts "player : " + @player_count.to_s
+    @player_count
   elsif win?(computer, player)
-    computer_count = computer_count + 1
-    puts "computer : " + computer_count.to_s
-    computer_count
+    @computer_count += 1
+    puts "computer : " + @computer_count.to_s
+    @computer_count
   else
     prompt("It's a tie!")
   end
 end
 
-def shortcut_check(choice)
-  case choice
-    when 'r','rock' then 'rock'
-    when 's','scissors' then 'scissors'
-    when 'p','paper' then 'paper'
-    when 'l','lizard' then 'lizard'
-    when 'sp','spock' then 'spock' 
-    end
+def total_count(player_count, computer_count)
+  if @player_count == 5
+    prompt("You Won!!")
+  elsif @computer_count == 5
+    prompt("Computer Won!")
+  end
 end
 
-def prompt(message)
-  Kernel.puts("=> #{message}")
-end
-player_count = 0
-computer_count = 0
-loop do
-  player_choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(',')}")
-    prompt_message = <<-MSG
+prompt_message = <<-MSG
     Please use shortcuts :
     r => rock
     p => paper
@@ -63,8 +59,12 @@ loop do
     l => lizard
     MSG
 
+loop do
+  player_choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(',')}")
+
     prompt(prompt_message)
-    
     choice = Kernel.gets().chomp()
     player_choice = shortcut_check(choice)
 
@@ -78,17 +78,11 @@ loop do
   computer_choice = VALID_CHOICES.sample
   prompt("You chose: #{player_choice} ; Computer chose: #{computer_choice}")
 
+  win_counter(player_choice, computer_choice)
 
-  counter(player_count, computer_count, player_choice, computer_choice)
-   if player_count == 5
-    prompt("You Won!")
-  elsif computer_count == 5
-    prompt("Computer Won!")
-  end
+  total_count(@player_count, @computer_count)
+
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
 end
-
-
-
