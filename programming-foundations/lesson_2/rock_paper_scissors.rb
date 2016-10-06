91240c2d64941767c980aac82628bba2f959a216
Neha <nehatandon1@gmail.com>
2016-10-06 17:54:05 +0530

    Implement rubocop suggestions

--- programming-foundations/lesson_2/rock_paper_scissors.rb
+++ programming-foundations/lesson_2/rock_paper_scissors.rb
@@ -3,10 +3,13 @@ require 'pry'
 
 VALID_CHOICES = %w(rock paper scissors lizard spock)
 POSITIVE_PLAY_AGAIN = %w(y yes)
-WINNING_CHOICES = {rock: ['scissors', 'lizard'], paper: %w(rock spock),
- scissors: %w(paper lizard),
+WINNING_CHOICES = {
+  rock: %w(scissors lizard),
+  paper: %w(rock spock),
+  scissors: %w(paper lizard),
   lizard: %w(spock paper),
-   spock: %w(scissors rock)}
+  spock: %w(scissors rock)
+}
 @player_count = 0
 @computer_count = 0
 
@@ -31,12 +34,10 @@ end
 def win_counter(player, computer)
   if win?(player, computer)
     @player_count += 1
-    puts "player : " + @player_count.to_s
-    @player_count
+    # puts "player : " + @player_count.to_s
   elsif win?(computer, player)
     @computer_count += 1
-    puts "computer : " + @computer_count.to_s
-    @computer_count
+    # puts "computer : " + @computer_count.to_s
   else
     prompt("It's a tie!")
   end
@@ -44,18 +45,16 @@ end
 
 def total_count(player_count, computer_count)
   prompt("Your score: #{player_count} ; Computer score: #{computer_count}")
+
   if @player_count == 5
     prompt("You Won!!")
     @player_count = 0
     @computer_count = 0
-    prompt("Your score: #{player_count} ; Computer score: #{computer_count}")
   elsif @computer_count == 5
     prompt("Computer Won!")
     @player_count = 0
     @computer_count = 0
-    prompt("Your score: #{player_count} ; Computer score: #{computer_count}")
   end
-
 end
 
 prompt_message = <<-MSG
@@ -91,12 +90,12 @@ loop do
   total_count(@player_count, @computer_count)
   prompt("Do you want to play again?")
   answer = ''
-  loop do 
+  loop do
     answer = Kernel.gets().chomp().downcase()
     case answer
-    when 'y','yes'
+    when 'y', 'yes'
       break
-    when 'n','no'
+    when 'n', 'no'
       prompt("Thanks for playing, GoodBye!")
       break
     else
