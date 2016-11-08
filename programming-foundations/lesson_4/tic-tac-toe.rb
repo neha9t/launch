0853c69375db34c68dd4aeace344f78569adee06
Neha Tandon <nehatandon1@gmail.com>
2016-11-08 12:20:28 +0530

    WIP

---
+++ programming-foundations/lesson_4/tic-tac-toe.rb
@@ -0,0 +1,45 @@
+=begin
+Tic Tac Toe is a 2 player game played on a 3x3 board. Each player takes a turn and
+marks a square on the board. First player to reach 3 squares in a row, including diagonals,
+wins. If all 9 squares are marked and no player has 3 squares in a row, then the game is a tie.
+
+1. Display the initial empty 3x3 board.
+2. Ask the user to mark a square.
+3. Computer marks a square.
+4. Display the updated board state.
+5. If winner, display winner.
+6. If board is full, display tie.
+7. If neither winner nor board is full, go to #2
+8. Play again?
+9. If yes, go to #1
+10. Good bye
+=end
+def display_board(brd)
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each {|num| new_board[num] = 'N'}
+  new_board
+end
+
+board = initialize_board
+
+display_board(board)
+
+
+
+
